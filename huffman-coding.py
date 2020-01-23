291682687c5ec71e30f93f6dd2b4cda4c16d3db7
ignatovegors <60047566+ignatovegors@users.noreply.github.com>
2020-01-23 23:17:39 +0300

    Update huffman-coding.py

--- huffman-coding.py
+++ huffman-coding.py
@@ -1,53 +1,47 @@
+from collections import Counter, namedtuple
+import heapq
+
+
+class Node(namedtuple('Node', ['left', 'right'])):
+    def walk(self, code, acc):
+        self.right.walk(code, acc + '0')
+        self.left.walk(code, acc + '1')
+
+
+class Leaf(namedtuple('Leaf', ['char'])):
+    def walk(self, code, acc):
+        code[self.char] = acc or '0'
+
+
 def huffman_coding(st):
-    freq_dic = dict()
-    code_dic = dict()
-    st_coded = ''
+    h = []
+    for ch, freq in Counter(st).items():
+        h.append((freq, len(h), Leaf(ch)))
 
-    for element in st:
-        if element in freq_dic:
-            freq_dic[element] += 1
-        else:
-            freq_dic[element] = 1
-            code_dic[element] = ''
+    heapq.heapify(h)
 
-    if len(freq_dic) == 1:
-        return [{st[0]: '0'}, '0' * len(st)]
-    else:
-        while len(freq_dic) >= 2:
-            keys_list = sorted(list(freq_dic.keys()), key=lambda i: freq_dic[i])
-            f0 = keys_list[0]
-            f1 = keys_list[1]
-            for element in f0:
-                code_dic[element] += '0'
-            for element in f1:
-                code_dic[element] += '1'
-            freq_dic[f0 + f1] = freq_dic[f0] + freq_dic[f1]
-            freq_dic.pop(f0)
-            freq_dic.pop(f1)
+    count = len(h)
+    while len(h) > 1:
+        freq1, _count1, left = heapq.heappop(h)
+        freq2, _count2, right = heapq.heappop(h)
+        heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
+        count += 1
 
-        for element in code_dic:
-            code_dic[element] = code_dic[element][::-1]
-
-        for element in st:
-            st_coded += code_dic[element]
-
-        return [code_dic, st_coded]
+    code = {}
+    if h:
+        [(_freq, _count, root)] = h
+        root.walk(code, '')
+    return code
 
 
 def main():
-    st = str(input())
-
-    res = huffman_coding(st)
-    res_dic = res[0]
-    res_st = res[1]
-
-    print(len(res_dic), len(res_st))
-
-    for element in res_dic:
-        print(element, end=': ')
-        print(res_dic[element])
-
-    print(res_st)
+    s = input()
+    code = huffman_coding(s)
+    encoded = "".join(code[ch] for ch in s)
+    print(len(code), len(encoded))
+    for key in sorted(code):
+        print('{}: {}'.format(key, code[key]))
+    print(encoded)
 
 
 if __name__ == '__main__':
